c2ece583173c5273538f6d94f74660c1b8500988
Tung Nguyen <shodydosh@gmail.com>
2024-08-14 14:26:00 +0700

    Done migrating

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Landing() {
+  return (
+    <div style={{ marginTop: '5rem' }}>
+      <h3>Landing Page Content</h3>
+    </div>
+  );
+}
+
+export default Landing;
