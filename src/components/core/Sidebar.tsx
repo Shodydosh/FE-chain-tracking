16890df2945e7e0a4d8f275cced64de41497b4de
Tung Nguyen <shodydosh@gmail.com>
2024-08-27 14:09:16 +0700

    feat: fixing sidebar according to route

--- src/components/core/Sidebar.tsx
+++ src/components/core/Sidebar.tsx
@@ -1,4 +1,6 @@
+'use client'
 import * as React from 'react'
+import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 import {
   Home,
@@ -12,22 +14,31 @@ import {
 
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip'
 
+const selected = ''
+
 const Sidebar: React.FC = () => {
+  const paths = usePathname()
+  const pathNames = paths.split('/').filter((path) => path)
+  const isActive: any = (href: string) => '/' + pathNames[0] === href
   return (
     <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
       <nav className="flex flex-col items-center gap-4 px-2 sm:py-4">
-        <Link
+        {/* <Link
           href="#"
           className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
         >
           <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
-          <span className="sr-only">Acme Inc</span>
-        </Link>
+          <span className="sr-only">FE-CHAIN-TRACKING</span>
+        </Link> */}
         <Tooltip>
           <TooltipTrigger asChild>
             <Link
               href="/home"
-              className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+              className={`group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full text-lg font-semibold md:h-8 md:w-8 md:text-base ${
+                isActive('/home')
+                  ? 'bg-primary text-primary-foreground'
+                  : 'text-muted-foreground'
+              }`}
             >
               <Home className="h-5 w-5" />
               <span className="sr-only">Home</span>
@@ -39,7 +50,11 @@ const Sidebar: React.FC = () => {
           <TooltipTrigger asChild>
             <Link
               href="/investigate"
-              className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+              className={`group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full text-lg font-semibold md:h-8 md:w-8 md:text-base ${
+                isActive('/investigate')
+                  ? 'bg-primary text-primary-foreground'
+                  : 'text-muted-foreground'
+              }`}
             >
               <ScanSearch className="h-5 w-5" />
               <span className="sr-only">Investigate</span>
@@ -51,7 +66,11 @@ const Sidebar: React.FC = () => {
           <TooltipTrigger asChild>
             <Link
               href="/graph"
-              className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+              className={`group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full text-lg font-semibold md:h-8 md:w-8 md:text-base ${
+                isActive('/graph')
+                  ? 'bg-primary text-primary-foreground'
+                  : 'text-muted-foreground'
+              }`}
             >
               <ChartNetwork className="h-5 w-5" />
               <span className="sr-only">Graph</span>
@@ -63,7 +82,11 @@ const Sidebar: React.FC = () => {
           <TooltipTrigger asChild>
             <Link
               href="/stories"
-              className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+              className={`group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full text-lg font-semibold md:h-8 md:w-8 md:text-base ${
+                isActive('/stories')
+                  ? 'bg-primary text-primary-foreground'
+                  : 'text-muted-foreground'
+              }`}
             >
               <ScrollText className="h-5 w-5" />
               <span className="sr-only">Stories</span>
@@ -74,8 +97,12 @@ const Sidebar: React.FC = () => {
         <Tooltip>
           <TooltipTrigger asChild>
             <Link
-              href="#"
-              className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+              href="/chart"
+              className={`group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full text-lg font-semibold md:h-8 md:w-8 md:text-base ${
+                isActive('/chart')
+                  ? 'bg-primary text-primary-foreground'
+                  : 'text-muted-foreground'
+              }`}
             >
               <ChartPie className="h-5 w-5" />
               <span className="sr-only">Chart</span>
